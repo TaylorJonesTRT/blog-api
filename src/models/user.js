1683b3f7f94abf7882bf85f053e463dc68446eff
Taylor Jones <taylor@taylorwjones.com>
2021-05-13 21:47:57 -0500

    finished user model

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose.Schema;
 
-const UserSchema = new Schema({});
+const UserSchema = new Schema({
+  username: { type: String, required: true, maxLength: 35 },
+  password: { type: String, required: true, minLength: 8 },
+});
 
 module.exports = mongoose.model('User', UserSchema);
