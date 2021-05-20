24ac0dbe1eff2d327b2ff5f79044c3ddf7694d06
Taylor Jones <taylor@taylorwjones.com>
2021-05-19 21:27:55 -0500

    added the routes in posts.js(route file) and starting on logic for those routes now

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,21 +1,22 @@
 const express = require('express');
-const Post = require('../models/post');
+const postController = require('../controllers/postController');
 
 const app = express();
 
-app.get('/', (req, res, next) => {
-  Post.find()
-    .sort([['date', 'descending']])
-    .exec((err, listPosts) => {
-      if (err) {
-        return next(err);
-      }
-      res.json({
-        posts: listPosts,
-      });
-    });
-});
+// GET request for creating a new blog post. This is before any route that
+// displays a blog post(using id)
+app.post('/create', postController.createNewPost);
 
-app.post('/', (req, res, next) => {});
+// POST request for deleting a blog post by its ID
+app.post('/post/:id/delete', postController.deletePostByID);
+
+// POST request for updating a blog post by its ID
+app.post('/post/:id/update', postController.updatePostByID);
+
+// GET request for retrieving a blog post and all it's data/information by its ID.
+app.get('/post/:id', postController.getPostByID);
+
+// GET request for retrieving every blog post in the database and their data/information
+app.get('/', postController.showAllPosts);
 
 module.exports = app;
--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,3 +1,27 @@
-const Post = require('../models/post');
 const async = require('async');
 const { body, validationResult } = require('express-validator');
+const Post = require('../models/post');
+const User = require('../models/user');
+
+// Return every post in the database showing most recent posted first
+exports.showAllPosts = function (req, res, next) {
+  Post.find()
+    .sort([['date', 'descending']])
+    .exec((err, listPosts) => {
+      if (err) {
+        return next(err);
+      }
+      return res.json({
+        posts: listPosts,
+      });
+    });
+};
+exports.createNewPost = function (req, res, next) {
+  return;
+};
+
+exports.deletePostByID = function (req, res, next) {};
+
+exports.updatePostByID = function (req, res, next) {};
+
+exports.getPostByID = function (req, res, next) {};
